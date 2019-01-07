743c651edab5e3e290b869c9db7fe67a3ba85686
Christopher Phillips <christopher.trevor.phillips@gmail.com>
2019-01-07 13:26:19 -0600

    Added comments to drawEnigma

--- EnigmaMachineSimulator.asm
+++ EnigmaMachineSimulator.asm
@@ -413,69 +413,69 @@ drawEnigma:
 	pushRegisters
 
 	# draw enigma background
-	li $t0, 0x663333
-	la $t1, framebuffer
-	addi $t2, $t1, 32768
+	li $t0, 0x663333	# background color (brown)
+	la $t1, framebuffer	# offset
+	addi $t2, $t1, 32768	# byte loop end condition
 
 colorBackGroundLoop:
-	sw $t0, 0($t1)
-	addi $t1, $t1, 4
-	blt $t1, $t2, colorBackGroundLoop
+	sw $t0, 0($t1)		# color pixel
+	addi $t1, $t1, 4	# move to next word
+	blt $t1, $t2, colorBackGroundLoop	# if byte end condition not met, loop
 	
 	# draw wire 1
-	li $t0, 0x00000000
+	li $t0, 0x00000000	# line color (black)
 	la $t1, framebuffer
-	addi $t1, $t1, 3732
-	addi $t2, $t1, 352
+	addi $t1, $t1, 3732	# offset
+	addi $t2, $t1, 352	# byte loop end condition
 
 drawLine1:
-	sw $t0, 0($t1)
-	addi $t1, $t1, 4
-	blt $t1, $t2, drawLine1
+	sw $t0, 0($t1)		# color pixel
+	addi $t1, $t1, 4	# move to next word
+	blt $t1, $t2, drawLine1	# if byte end condition not met, loop
 
 	# draw wire 2
-	li $t0, 0x00000000
+	li $t0, 0x00000000	# line color (black)
 	la $t1, framebuffer
-	addi $t1, $t1, 8340
-	addi $t2, $t1, 240
+	addi $t1, $t1, 8340	# offset
+	addi $t2, $t1, 240	# byte loop end condition
 
 drawLine2:
-	sw $t0, 0($t1)
-	addi $t1, $t1, 4
-	blt $t1, $t2, drawLine2
+	sw $t0, 0($t1)		# color pixel
+	addi $t1, $t1, 4	# move to next word
+	blt $t1, $t2, drawLine2	# if byte end condition not met, loop
 
 	# draw wire 3
-	li $t0, 0x00000000
+	li $t0, 0x00000000	# line color (black)
 	la $t1, framebuffer
-	addi $t1, $t1, 4592
-	addi $t2, $t1, 32768
+	addi $t1, $t1, 4592	# offset
+	addi $t2, $t1, 32768	# byte loop end condition
 
 drawLine3:
-	sw $t0, 0($t1)
-	addi $t1, $t1, 512
-	blt $t1, $t2, drawLine3
+	sw $t0, 0($t1)		# color pixel
+	addi $t1, $t1, 512	# move to next word
+	blt $t1, $t2, drawLine3	# if byte end condition not met, loop
 	
 	# draw wire 4
-	li $t0, 0x00000000
+	li $t0, 0x00000000	# color pixel
 	la $t1, framebuffer
-	addi $t1, $t1, 9088
-	addi $t2, $t1, 2560
+	addi $t1, $t1, 9088	# offset
+	addi $t2, $t1, 2560	# byte loop end condition
 
 drawLine4:
-	sw $t0, 0($t1)
-	addi $t1, $t1, 512
-	blt $t1, $t2, drawLine4
+	sw $t0, 0($t1)		# color pixel
+	addi $t1, $t1, 512	# move to next word
+	blt $t1, $t2, drawLine4	# if byte end condition not met, loop
 	
 	# draw wire 5
-	li $t0, 0x00000000
+	li $t0, 0x00000000	# color pixel
 	la $t1, framebuffer
-	addi $t1, $t1, 11636
-	addi $t2, $t1, 28
+	addi $t1, $t1, 11636	# offset
+	addi $t2, $t1, 28	# byte loop end condition
 
 drawLine5:
-	sw $t0, 0($t1)
-	addi $t1, $t1, 4
-	blt $t1, $t2, drawLine5
+	sw $t0, 0($t1)		# color pixel
+	addi $t1, $t1, 4	# move to next word
+	blt $t1, $t2, drawLine5	# if byte end condition not met, loop
 	
 	# pop s-registers from the stack
 	popRegisters
